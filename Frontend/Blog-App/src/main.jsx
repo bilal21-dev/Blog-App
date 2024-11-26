d2d6d42168b0571bde56052af273a10763d26413
Abdullah-Khawar <handsomebilal877@gmail.com>
2024-11-26 11:34:20 +0500

    remove app.jsx

--- Frontend/Blog-App/src/main.jsx
+++ Frontend/Blog-App/src/main.jsx
@@ -1,10 +1,35 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Intro from "./Components/Intro.jsx";
+import Login from "./Components/Register/Login.jsx";
+import SignUp from "./Components/Register/Signup.jsx";
+import Post from "./Components/Home/Post.jsx";
+import Layout from "./Layout.jsx";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      {/*(intro screen) */}
+      <Route index element={<Intro />} />
+      {/* other routes */}
+      <Route path="home" element={<Post />} />
+      <Route path="about" element={<h1>Hi</h1>} />
+      <Route path="login" element={<Login />} />
+      <Route path="signup" element={<SignUp />} />
+    </Route>
+  )
+);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
